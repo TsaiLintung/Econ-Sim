4c8de92dd48ee1e75fe43054d7022bec0a738ea5
TsaiLintung <101155567+TsaiLintung@users.noreply.github.com>
2022-06-14 11:17:54 +0800

    a dino that runs right constantly

--- sketch.js
+++ sketch.js
@@ -1,5 +1,16 @@
+let img;
+let windowHeight, windowWidth;
+let x;
+
+function preload() {
+  img = loadImage('assets/graphics/player/animation/dino-0.png')
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(window.innerWidth, window.innerHeight);
+  windowHeight = window.innerHeight;
+  windowWidth = window.innerWidth; 
+  x=0
 }
 
 function draw() {
@@ -7,4 +18,13 @@ function draw() {
   if(mouseIsPressed){pressed();}
   else{notpressed();}
   ellipse(mouseX,mouseY,80,80);
+  image(img, x,windowHeight/2);
+  x += 5;
+  if (x >= windowWidth){x=0;}
+}
+
+function windowResized() {
+  resizeCanvas(window.innerWidth, window.innerHeight);
+  windowHeight = window.innerHeight;
+  windowWidth = window.innerWidth;  
 }
