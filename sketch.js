1883712fce2baa0da4aafe5882372e1f6a6a1ce8
TsaiLintung <101155567+TsaiLintung@users.noreply.github.com>
2022-06-14 15:26:42 +0800

    Create agent calss

--- sketch.js
+++ sketch.js
@@ -1,52 +1,55 @@
 
 //setup the economc parameters
 const PARAMS = {
-  speed: 5
+  speed: 5,
+  fps:60,
+  bgColor: "#FFFFF0"
 };
 let x = 0;
 
 // setup the system parameters
 let windowHeight, windowWidth;
-let fps = 60,
-fontSize = 50,
-caption = "Econ Sim",
-bgColor = "#FFFFF0";
 
 
 //setup the paths for assets
-let dinoPath;
-let dinoPic;
-
+const PATHS = {
+  'dino': 'assets/graphics/player/animation/dino-0.png'
+};
 
 //setup UI elements
 var pane, sys;
 
 // this is called before setsup
-function preload() {
-  dinoPath = 'assets/graphics/player/animation/dino-0.png';
-}
+function preload() {}
 
 //this is called when the sim started.
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
-  frameRate(fps);
+  frameRate(PARAMS.fps);
   windowHeight = window.innerHeight;
   windowWidth = window.innerWidth; 
-  dinoPic = loadImage(dinoPath);
-  
+
+  agent = new Agent(10, 500, PARAMS.speed, PATHS.dino);
+
   pane = new Tweakpane.Pane();
   sys = pane.addFolder({title:"System"});
-  sys.addInput(PARAMS, 'speed', {min :0, max : 100});
+  sys.addInput(PARAMS, 'speed', {min :0, max : 10});
+  sys.addInput(PARAMS, 'fps', {min :30, max : 120});
+
+  focusAgent = pane.addFolder({title:"Focus Agent"});
+  focusAgent.addMonitor(agent.pos, 'x')
+  focusAgent.addMonitor(agent.pos, 'y')
 }
 
 //this is called every frame
 function draw() {
-  background(bgColor);
-  image(dinoPic, x, windowHeight/2);
-  x += PARAMS['speed'];
-  if (x >= windowWidth){x=0;}
-  frameRate(fps);
+  background(PARAMS.bgColor);
 
+  agent.update();
+  image(agent.pic, agent.pos.x, agent.pos.y);
+  
+  frameRate(PARAMS.fps);
+  agent.speed = PARAMS.speed;
   pane.refresh();
 }
 
